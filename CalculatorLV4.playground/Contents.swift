17cf62ba2896f7c1958a9101bf848e3a227bd8b6
Bread-kn72 <ordinarytom72@gmail.com>
2024-03-07 17:43:32 +0900

    2번째 과제저장 lv4추가

--- CalculatorLV4.playground/Contents.swift
+++ CalculatorLV4.playground/Contents.swift
@@ -3,7 +3,7 @@
 // AbstractOperation 클래스
 class AbstractOperation {
     func operate(firstNumber: Double, secondNumber: Double) -> Any {
-        return 0
+            return 0
     }
 }
 
@@ -18,30 +18,35 @@ class Calculator {
     func calculate(firstNumber: Double, secondNumber: Double) -> Any {
         return operation.operate(firstNumber: firstNumber, secondNumber: secondNumber)
     }
+    
+    func changeOperation(operation: AbstractOperation) {
+        self.operation = operation
+    }
 }
 
-// Addition 클래스
+
+// AddOperation 클래스
 class AddOperation: AbstractOperation {
     override func operate(firstNumber: Double, secondNumber: Double) -> Any {
         return firstNumber + secondNumber
     }
 }
 
-// Subtraction 클래스
+// SubtractOperation 클래스
 class SubtractOperation: AbstractOperation {
     override func operate(firstNumber: Double, secondNumber: Double) -> Any {
         return firstNumber - secondNumber
     }
 }
 
-// Multiplication 클래스
+// MultiplyOperation 클래스
 class MultiplyOperation: AbstractOperation {
     override func operate(firstNumber: Double, secondNumber: Double) -> Any {
         return firstNumber * secondNumber
     }
 }
 
-// Division 클래스
+// DivideOperation 클래스
 class DivideOperation: AbstractOperation {
     override func operate(firstNumber: Double, secondNumber: Double) -> Any {
         if secondNumber == 0 {
@@ -52,15 +57,17 @@ class DivideOperation: AbstractOperation {
     }
 }
 
-
 // Calculator 클래스의 인스턴스 생성
-let calculator = Calculator(operation: DivideOperation())
+let calculator = Calculator(operation: AddOperation())
 
 // 계산 수행
 let addResult = calculator.calculate(firstNumber: 10, secondNumber: 5)
+calculator.changeOperation(operation: SubtractOperation())
 let subtractResult = calculator.calculate(firstNumber: 10, secondNumber: 5)
+calculator.changeOperation(operation: MultiplyOperation())
 let multiplyResult = calculator.calculate(firstNumber: 10, secondNumber: 5)
-let divideResult = calculator.calculate(firstNumber: 10, secondNumber: 0)
+calculator.changeOperation(operation: DivideOperation())
+let divideResult = calculator.calculate(firstNumber: 10, secondNumber: 5)
 
 print("addResult : \(addResult)")
 print("subtractResult : \(subtractResult)")
